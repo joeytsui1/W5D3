f629677babb2d849537d6b829b86ede22f5e48bc
Joey Tsui <joeytsui131@gmail.com>
2022-12-07 14:51:15 -0500

    a

--- Questions_Database.rb
+++ Questions_Database.rb
@@ -17,10 +17,29 @@ class QuestionsDatabase < SQLite3::Database
 end
 
 class Questions
-    def initialize
-        
+
+    def self.all
+        data = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
+        data.map {|datum| Questions.new(datum)}
     end
 
+    def initialize(option)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end 
+    
+    def create
+        raise "#{self} already in database" if @id
+        QuestionsDBConnection.instance.execute(<<-SQL, @title, @body, @author_id)
+            INSERT INTO 
+                questions (title, body, author_id)
+            VALUES
+                (?, ?, ?)
+        SQL
+        @id = QuestionsDBConnection.instance.last_insert_row_id
+    end
     def find_by_id
 
     end
